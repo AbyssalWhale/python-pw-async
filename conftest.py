77b76498fae307d16e27345dda060c594fdf14ab
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2024-06-10 16:43:57 +0200

    Update conftest.py
    adding __create_test_results_folder

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from helpers.test_run_config import TestRunConfig
 from playwright.async_api import async_playwright
 
@@ -6,10 +7,13 @@ from playwright.async_api import async_playwright
 class Fixtures:
     __test_run_config = None
     __page = None
+    test_results_dir = None
 
     @pytest.fixture(scope="session", autouse=True)
     async def one_time_setup(self):
         self.__test_run_config = await TestRunConfig.init()
+        self.test_results_dir = await self.__create_test_results_folder()
+
         playwright = await async_playwright().start()
         browser = await playwright.chromium.launch(headless=False)
         context = await browser.new_context()
@@ -20,6 +24,13 @@ class Fixtures:
 
         await browser.close()
         await playwright.stop()
+
+    async def __create_test_results_folder(self):
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+        test_results_dir = os.path.join(current_dir, "test_results")
+        if not os.path.exists(test_results_dir):
+            os.makedirs(test_results_dir, exist_ok=True)
+        return test_results_dir
 
 
 class TestExample(Fixtures):
